66925fb1d1eca764437fa93532aefd440f1aafe8
Viktoria Navasardyan <88617144+ViktoryaNavasardyan@users.noreply.github.com>
2021-10-01 00:49:41 +0400

    Add files via upload

---
+++ BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortClasses.Models
+{
+    class BubbleSort
+    {
+        public static double[] Sort(double[] arr)
+        {
+            //copy array
+            double[] array = arr;
+            int num = array.Length;
+            for (int i = 0; i < num - 1; i++)
+            {
+                for (int j = 0; j < num - i - 1; j++)
+                    if (array[j] > array[j + 1])
+                    {
+                        // swap tmp and arr[i]
+                        double tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                    }
+            }
+            return array;
+        }
+    }
+}
---
+++ InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortClasses.Models
+{
+    class InsertionSort
+    {
+        public static double[] Sort(double[] array)
+        {
+            int n = array.Length;
+            for (int i = 1; i < n; i++)
+            {
+                double value = array[i];
+                bool flag = false;
+                for (int j = i - 1; j >= 0 && !flag;)
+                {
+                    if (value < array[j])
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                        array[j + 1] = value;
+                    }
+                    else flag = true;
+                }
+            }
+            return array;
+        }
+    }
+}
